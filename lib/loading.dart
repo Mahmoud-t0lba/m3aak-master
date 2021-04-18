1f493b736c7c8db5177fd867db7b9ecad335d69b
Mahmoud-t0lba <mahmoudt0lba0111@gmail.com>
2021-04-17 19:03:11 -0700

    push

---
+++ lib/loading.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+// ignore: camel_case_types
+class loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: SpinKitChasingDots(
+          color: Colors.blue,
+          size: 50,
+        ),
+      ),
+    );
+  }
+}
